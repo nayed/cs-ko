06e9d66a2d0f20905992e765d21270a82a59e4e3
Nayed Saïd Ali <nayed.saidali@gmail.com>
2017-02-02 14:18:35 +0100

    basic search

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,48 @@
 import React, { Component } from 'react'
 import '../sass/App.sass'
 
+const list = [
+  {
+    objectID: 1,
+    name: "PHP",
+  },
+  {
+    objectID: 2,
+    name: "Javascript",
+  }
+]
+
+
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      list,
+      searchTerm: ''
+    }
+
+    this.onSearchChange = this.onSearchChange.bind(this)
+  }
+
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value })
+  }
+
   render() {
+    const isSearched = (searchTerm) => (item) =>
+      !searchTerm || item.name.toLowerCase().includes(searchTerm.toLowerCase())
+    const { searchTerm, list } = this.state
     return (
       <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <form>
+          <input type="text" value={searchTerm} onChange={this.onSearchChange} />
+        </form>
+        { list.filter(isSearched(searchTerm)).map(item =>
+          <div key={item.objectID}>
+            <span>{item.name}</span>
+          </div>
+        )}
       </div>
     )
   }

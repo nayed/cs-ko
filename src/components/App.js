1580936d3b341a573cc26cf1bffc9879b138549a
Nayed Saïd Ali <nayed.saidali@gmail.com>
2017-02-03 18:02:30 +0100

    .

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,10 @@
 import React, { Component } from 'react'
-import '../sass/App.sass'
+import '../style/App.sass'
 import Navigation from './Navigation'
 import Search from './Search'
 
 class App extends Component {
-
   render() {
-    // const isSearched = (searchTerm) => (item) =>
-    //   !searchTerm || item.name.toLowerCase().includes(searchTerm.toLowerCase())
-    // const { searchTerm, language } = this.state
-    // return (
-    //   <div className="App">
-    //
-    //   </div>
-    // )
     return (
       <div>
         <Navigation />
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,22 +1,43 @@
 import React, { Component } from 'react'
-import { Nav, NavItem, NavLink } from 'reactstrap'
+import FA from 'react-fontawesome'
+import '../style/Navigation.sass'
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 
 class Navigation extends Component {
+  constructor(props) {
+    super(props)
+
+    this.toggle = this.toggle.bind(this)
+    this.state = {
+      isOpen: false
+    }
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    })
+  }
   render() {
     return (
       <div>
-        <Nav>
-          <NavItem>
-            <NavLink href="#">CS-KO</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="#">Link</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="#">Another Link</NavLink>
-          </NavItem>
-        </Nav>
-        <hr />
+        <Navbar color="faded" light toggleable className="navigation">
+          <NavbarToggler right onClick={this.toggle} />
+          <NavbarBrand href="/"><span>Coding Style Koolog </span>
+            <FA
+              name='book'
+            />
+          </NavbarBrand>
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto nav navbar-right" navbar>
+              <NavItem>
+                <NavLink href="#">Link</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#">Link</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
       </div>
     )
   }

1473a21b048fd0f3d92cf9f0b3a6114eb1b44b1c
Nayed Saïd Ali <nayed.saidali@gmail.com>
2017-02-03 07:47:28 +0100

    .

--- src/components/App.js
+++ src/components/App.js
@@ -3,35 +3,7 @@ import '../sass/App.sass'
 import Navigation from './Navigation'
 import Search from './Search'
 
-const language = [
-  {
-    objectID: 1,
-    name: "PHP",
-    icone: ''
-  },
-  {
-    objectID: 2,
-    name: "Javascript",
-    icone: ''
-  }
-]
-
-
 class App extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      language,
-      searchTerm: ''
-    }
-
-    this.onSearchChange = this.onSearchChange.bind(this)
-  }
-
-  onSearchChange(event) {
-    this.setState({ searchTerm: event.target.value })
-  }
 
   render() {
     // const isSearched = (searchTerm) => (item) =>
@@ -39,21 +11,18 @@ class App extends Component {
     // const { searchTerm, language } = this.state
     // return (
     //   <div className="App">
-    //     <form>
-    //       <input type="text" value={searchTerm} onChange={this.onSearchChange} />
-    //     </form>
-    //     { language.filter(isSearched(searchTerm)).map(item =>
-    //       <div key={item.objectID}>
-    //         <span>{item.name}</span>
-    //       </div>
-    //     )}
+    //
     //   </div>
     // )
     return (
       <div>
         <Navigation />
-        <div className="container">
-          <Search />
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-4">
+              <Search />
+            </div>
+          </div>
         </div>
       </div>
     )
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,48 @@
 import React, { Component } from 'react'
 import {  } from 'reactstrap'
 
+const language = [
+  {
+    objectID: 1,
+    name: "PHP",
+    icone: ''
+  },
+  {
+    objectID: 2,
+    name: "Javascript",
+    icone: ''
+  }
+]
+
 class Search extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      language,
+      searchTerm: ''
+    }
+
+    this.onSearchChange = this.onSearchChange.bind(this)
+  }
+
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value })
+  }
   render() {
+    const isSearched = (searchTerm) => (item) =>
+      !searchTerm || item.name.toLowerCase().includes(searchTerm.toLowerCase())
+    const { searchTerm, language } = this.state
     return (
       <div>
-        <form></form>
+      <form>
+        <input type="text" value={searchTerm} onChange={this.onSearchChange} />
+      </form>
+      { language.filter(isSearched(searchTerm)).map(item =>
+        <ul key={item.objectID}>
+          <li>{item.name}</li>
+        </ul>
+      )}
       </div>
     )
   }

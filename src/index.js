d8bde4ed7981a9bf529503557342be02e071b69a
Nayed Saïd Ali <nayed.saidali@gmail.com>
2017-02-04 18:05:30 +0100

    .

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import './style/index.sass'
 import 'normalize.css/normalize.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootswatch/flatly/bootstrap.css'
-import 'font-awesome/css/font-awesome.css'
 
 ReactDOM.render(
   <App />,
